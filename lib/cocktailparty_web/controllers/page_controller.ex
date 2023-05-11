380c0240528e9c515e7c4ebd3a393d883af8f752
Jean-Louis Huynen <huynenjl@gmail.com>
2023-05-11 11:55:17 +0200

    chg: [auth]

--- lib/cocktailparty_web/controllers/page_controller.ex
+++ lib/cocktailparty_web/controllers/page_controller.ex
@@ -4,7 +4,6 @@ defmodule CocktailpartyWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    # render(conn, :home, layout: false)
     render(conn, :home, layout: false)
   end
 end
