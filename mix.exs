68105f115ec7241ad1fda46787508e6b0b9a9ecf
Jean-Louis Huynen <huynenjl@gmail.com>
2024-01-15 13:49:20 +0100

    chg: [mix] gen_stmp

--- mix.exs
+++ mix.exs
@@ -60,6 +60,7 @@ defmodule Cocktailparty.MixProject do
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
+      {:gen_smtp, "~> 1.1"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
